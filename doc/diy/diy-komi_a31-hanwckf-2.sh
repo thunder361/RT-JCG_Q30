728ba17e671f0dc58c870208d8b2a5444a2c3026
thunder361 <73006038+thunder361@users.noreply.github.com>
2023-05-31 17:34:02 +0800

    Create diy-komi_a31-hanwckf-2.sh

---
+++ doc/diy/diy-komi_a31-hanwckf-2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+#修改默认IP
+sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate   # 定制默认IP
+#sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='OpenWrt By thunder361 ($(date +%Y-%m-%d %H:%M)) '/g" package/base-files/files/etc/openwrt_release
+# 移除重复软件包
+#rm -rf feeds/luci/themes/luci-theme-argon
+
+# 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Themes
+#git clone --depth 1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+#git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+
+# 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
+#sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
